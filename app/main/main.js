df50fc6a6041ae992e7c114d0eec6cac9bca2395
Pham Thi Cam Duyen <fantasticJilove@gmail.com>
2016-08-25 20:46:00 +0700

    re-organise dir

---
+++ app/main/main.js
@@ -0,0 +1,7 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var my_component_1 = require('../components/my-component');
+platform_browser_dynamic_1.bootstrap(my_component_1.MyComponent).catch(function (err) {
+    console.log(err);
+});
+//# sourceMappingURL=main.js.map
---
+++ app/main/main.ts~
@@ -0,0 +1,6 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { MyComponent } from '../components/my-component';
+
+bootstrap(MyComponent).catch((err) => {
+	console.log(err);
+});
